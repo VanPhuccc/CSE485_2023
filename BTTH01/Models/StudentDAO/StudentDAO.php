ae8dd0db7b90c7c862a9dd181d719776dddbeb3a
PQMinh47 <84690745+PQMinh47@users.noreply.github.com>
2023-05-15 00:51:48 +0700

    Add files via upload

---
+++ BTTH01/Models/StudentDAO/StudentDAO.php
@@ -0,0 +1,42 @@
+<?
+class StudentDAO {
+    private $students = array();
+  
+    public function create($student) {
+      array_push($this->students, $student);
+    }
+  
+    public function read($id) {
+      foreach ($this->students as $student) {
+        if ($student->getId() == $id) {
+          return $student;
+        }
+      }
+      return null;
+    }
+  
+    public function update($id, $student) {
+      foreach ($this->students as $key => $value) {
+        if ($value->getId() == $id) {
+          $this->students[$key] = $student;
+          return true;
+        }
+      }
+      return false;
+    }
+  
+    public function delete($id) {
+      foreach ($this->students as $key => $value) {
+        if ($value->getId() == $id) {
+          unset($this->students[$key]);
+          return true;
+        }
+      }
+      return false;
+    }
+  
+    public function getAll() {
+      return $this->students;
+    }
+}
+?>
