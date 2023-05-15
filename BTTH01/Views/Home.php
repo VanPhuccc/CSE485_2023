4b086dacd7ae091ad16849e80e5d081f15fa5e3a
TrinhPhuc <71745283+bibi1861@users.noreply.github.com>
2023-05-15 19:05:05 +0700

    update home.php
    upload code trang chu

--- BTTH01/Views/Home.php
+++ BTTH01/Views/Home.php
@@ -1,4 +1,73 @@
-// connect 
+<?php
+$type     = 'mysql';                 // Type of database
+$server   = 'localhost';             // Server the database is on
+$db       = 'project09';             // Name of the database
+$port     = '';                      // Port is usually 8889 in MAMP and 3306 in XAMPP
+$charset  = 'utf8mb4';               // UTF-8 encoding using 4 bytes of data per character
 
+$username = 'root';         // Enter YOUR username here
+$password = '';         // Enter YOUR password here
 
-// display 
+$options  = [                        // Options for how PDO works
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];                                                                  // Set PDO options
+
+// DO NOT CHANGE ANYTHING BENEATH THIS LINE
+$dsn = "$type:host=$server;dbname=$db;port=$port;charset=$charset"; // Create DSN
+try {                                                               // Try following code
+    $pdo = new PDO($dsn, $username, $password, $options);           // Create PDO object
+} catch (PDOException $e) {                                         // If exception thrown
+    throw new PDOException($e->getMessage(), $e->getCode());        // Re-throw exception
+}
+
+$stmt = $pdo->prepare('select *from students limit 10');
+$stmt->execute([$id,$fullname]);
+
+$students = $stmt->fetchAll();
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+</head>
+<body>
+    <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">Mã sinh viên</th>
+      <th scope="col">Họ và tên</th>
+      <th scope="col">Sửa</th>
+      <th scope="col">Xóa</th>
+    </tr>
+  </thead>
+  <tbody>
+    <?php
+        foreach($students as $student){
+    ?>
+            <tr>
+                <th scope="row"><?= $student[0]; ?></th>
+                <td><?= $student[1]; ?></td>
+                <td>
+                    <a href="edit.php?id=<?= $student[0]; ?>"><i class="bi bi-pencil-square"></i></a>
+                </td>
+                <td>
+                    <a href="del.php?id=<?= $student[0]; ?>"><i class="bi bi-trash3-fill"></i></a>
+                </td>
+            </tr>
+    <?php
+        }
+    ?>
+  </tbody>
+</table>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+</body>
+</html>
