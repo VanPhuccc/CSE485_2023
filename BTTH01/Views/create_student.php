aa5db0dcc86b8a1870cbf5069f61a0e3aea22a72
velbath <132930424+velbath@users.noreply.github.com>
2023-05-16 10:11:47 +0700

    Update create_student.php

--- BTTH01/Views/create_student.php
+++ BTTH01/Views/create_student.php
@@ -1,1 +1,10 @@
-
+<form method="POST">
+<label for="id">ID:</label>
+<input type="number" id="id" name="id" required>
+<br>
+<label for="name">Name:</label>
+<input type="text" id="name" name="name" required>
+<br>
+<label for="age">Age:</label>
+<input type="number" id="age" name="age" required>
+<br>
