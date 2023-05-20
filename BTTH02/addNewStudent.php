facdea710f0f77a6cb7fe71dcec16cdd212f1c35
velbath <132930424+velbath@users.noreply.github.com>
2023-05-20 13:14:22 +0700

    Update addNewStudent.php

---
+++ BTTH02/addNewStudent.php
@@ -0,0 +1,20 @@
+<form method="POST">
+<label for="id">ID:</label>
+<input type="number" id="id" name="id" required>
+<br>
+<label for="name">Name:</label>
+<input type="text" id="name" name="name" required>
+<br>
+<label for="email">Email:</label>
+<input type="text" id="age" name="age" required>
+<br>
+<label for="bdate">Date of birth:</label>
+<input type="text" id="bdate" name="bdate" required>
+<br>
+<label for="phone">Phone number:</label>
+<input type="number" id="phone" name="phone" required>
+<br>
+<input type="submit" value="Create">
+</form>
+
+<p><?php echo $message; ?></p>
