9ed77dca3ae1d9d3c4ba2a8859e476f1aeff170e
velbath <132930424+velbath@users.noreply.github.com>
2023-05-21 00:22:46 +0700

    Update addNewStudent.php

--- BTTH02/addNewStudent.php
+++ BTTH02/addNewStudent.php
@@ -24,3 +24,27 @@
     else
         echo "Add failed";
 ?>
+
+$msg="";
+if(isset($_POST['sub'])){
+	
+	$id = $_POST['id'];
+	$name = $_POST['name'];
+	$email = $_POST['email'];
+	$bdate = $_POST['bdate$bdate'];
+	$phone = $_POST['phone'];
+	
+	$query = mysqli_query($con,"INSERT INTO `student` (`id`, `name`, `email`, `bdate$bdate`, `phone`) VALUES (NULL,'$name','$email','$bdate','$phone')");
+	
+	if($query){
+		$sql = mysqli_query($con,"SELECT * FROM `student` WHERE `id` = '$id' AND `phone` = '$phone'");
+		$r = mysqli_fetch_array($sql);
+		$r_id = $r['id'];
+		mysqli_query($con,"INSERT INTO `student`(`id`, `name`, `phone`) VALUES ('NULL','TEXT','Not Set')");
+		$msg= '<div class="alert alert-success"><strong>Success!</strong> Added New Student.</div>';
+	}else{
+		$msg= '<div class="alert alert-danger"><strong>Fail!</strong> Try Again.</div>';
+	}
+}
+
+?>
