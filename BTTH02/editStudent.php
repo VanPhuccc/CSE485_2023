8b69460c4d95a104561f2ca93cf906af3d48fe57
Khanh Nguyen <132930424+khanhnyuen@users.noreply.github.com>
2023-05-21 23:23:21 +0700

    Update editStudent.php

--- BTTH02/editStudent.php
+++ BTTH02/editStudent.php
@@ -1,1 +1,15 @@
-
+<form method="post" action="editpost.php">
+        <input type="hidden" name="id" value="<?php echo $attendance['id'] ?>" />
+        <div class="form-group">
+            <label for="id">Id</label>
+            <input type="number" class="form-control" value="<?php echo $attendance['id'] ?>" id="id" name="id">
+        </div>
+        <div class="form-group">
+            <label for="name">Name</label>
+            <input type="text" class="form-control" value="<?php echo $attendance['name'] ?>" id="name" name="name">
+        </div>
+        <div class="form-group">
+            <label for="bdate">Date Of Birth</label>
+            <input type="text" class="form-control" value="<?php echo $attendance['dateofbirth'] ?>" id="dbate" name="dbate">
+        </div>
+</form>
