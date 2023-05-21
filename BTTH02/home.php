185caea658985167c9052fcd344060950daaddd8
Văn Phúc <71745283+bibi1861@users.noreply.github.com>
2023-05-21 22:09:59 +0700

    update home.php

--- BTTH02/home.php
+++ BTTH02/home.php
@@ -1,3 +1,12 @@
+<?php
+require 'include/DBconnect.php'; 
+require 'include/function.php';
+$sql = "Select *from students";
+$students = pdo($pdo,$sql)->fetchAll();
+
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,67 +14,53 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Quản lý sinh viên</title>
-</head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+  </head>
 <body>
 <div class = "container">
-    <h3> Quản lý sinh viên </h3>
+    <h3 style="font-family:serif;text-align:center">Students List </h3>
 
 </div>
 
-<form>
-  <div class="row mb-3">
-    <label for="inputEmail3" class="col-sm-2 col-form-label">Họ và tên</label>
-    <div class="col-sm-10">
-      <input type="email" class="form-control" id="inputEmail3">
-    </div>
-  </div>
-  <div class="row mb-3">
-    <label for="inputPassword3" class="col-sm-2 col-form-label">Giới tính</label>
-    <div class="col-sm-10">
-      <input type="password" class="form-control" id="inputPassword3">
-    </div>
-  </div>
-  <div class="row mb-3">
-    <label for="inputPassword3" class="col-sm-2 col-form-label">Email</label>
-    <div class="col-sm-10">
-      <input type="password" class="form-control" id="inputPassword3">
-    </div>
-  </div>
-  <fieldset class="row mb-3">
-    <legend class="col-form-label col-sm-2 pt-0">Radios</legend>
-    <div class="col-sm-10">
-      <div class="form-check">
-        <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios1" value="option1" checked>
-        <label class="form-check-label" for="gridRadios1">
-          First radio
-        </label>
-      </div>
-      <div class="form-check">
-        <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios2" value="option2">
-        <label class="form-check-label" for="gridRadios2">
-          Second radio
-        </label>
-      </div>
-      <div class="form-check disabled">
-        <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios3" value="option3" disabled>
-        <label class="form-check-label" for="gridRadios3">
-          Third disabled radio
-        </label>
-      </div>
-    </div>
-  </fieldset>
-  <div class="row mb-3">
-    <div class="col-sm-10 offset-sm-2">
-      <div class="form-check">
-        <input class="form-check-input" type="checkbox" id="gridCheck1">
-        <label class="form-check-label" for="gridCheck1">
-          Example checkbox
-        </label>
-      </div>
-    </div>
-  </div>
-  <button type="submit" class="btn btn-primary">Sign in</button>
-</form>
+<table class="table table-bordered text-center" style="font-family:monospace;" >
+  <thead>
+    <tr class="text-white" style="background-color:#7393B3">
+      <th scope="col">ID</th>
+      <th scope="col">Name</th>
+      <th scope="col">Birthday</th>
+      <th scope="col">Gender</th>
+      <th scope="col">Email</th>
+      <th scope="col">Phone number</th>
+      <th scope="col" style="color:lightgreen">Edit</th>
+      <th scope="col" style="color:red">Remove</th>
+    </tr>
+  </thead>
+  <thead>
+    <tr>
+    <?php
+      foreach($students as $students)
+      {
+    ?>
+      <th><?php echo $students['sv_id']; ?></th>
+      <th><?php echo $students['sv_name']; ?></th>
+      <th><?php echo $students['sv_bdate']; ?></th>
+      <th><?php echo $students['gender']; ?></th>
+      <th><?php echo $students['sv_email']; ?></th>
+      <th><?php echo $students['sv_phone']; ?></th>
+      <td>
+        <a href="editStudent.php"><i class="bi bi-pencil-square"></i></a>
+      </td>
+      <td>
+        <a href="removeStudent.php"><i class="bi bi-file-earmark-x"></i></a>
+      </td>
+    </tr>
+    <?php
+      }
+    ?>
+  </thead>
+</table>
 </body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
 </html>
 
