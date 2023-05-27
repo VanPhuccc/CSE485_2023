6fa4a6752b76ac90efefc84b465dca934c1c8497
Văn Phúc <71745283+VanPhuccc@users.noreply.github.com>
2023-05-27 22:02:30 +0700

    Upload models file

--- BTTH03/app/models/Article.php
+++ BTTH03/app/models/Article.php
@@ -1,1 +1,25 @@
+<?php
+
+class Article{
+    private $title;
+    private $summary;
+    private $content;
+    private $author_name;
+
+    public function __construct($title, $summary, $content, $author_name){
+        $this->title = $title;
+        $this->summary = $summary;
+        $this->content = $content;
+        $this->author_name= $author_name;
+    }
+
+    public function getTitle(){
+        return $this->title;
+    }
+
+    public function setTitle($title){
+        $this->title = $title;
+    }
+
+}
 
