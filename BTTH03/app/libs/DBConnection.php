bd7f2cd256ef1ee3ac3ac45709d60c936775b249
Văn Phúc <71745283+VanPhuccc@users.noreply.github.com>
2023-05-27 22:20:17 +0700

    complete DBConnection file

--- BTTH03/app/libs/DBConnection.php
+++ BTTH03/app/libs/DBConnection.php
@@ -1,1 +1,30 @@
+<?php
+class DBConnection{
+    private $host;
+    private $user;
+    private $dbname;
+    private $pass;
+    private $conn;
 
+    public function __construct($host,$user,$dbname,$pass)
+    {
+        $this->host = DB_HOST;
+        $this->user = DB_USER;
+        $this->pass = DB_PASS;
+        $this->dbname = DB_NAME;
+
+        try{
+            $this->conn = new PDO("mysql:host={$this->host};dbname={$this->dbname},$this->user,$this->pass");
+        }catch(PDOException $e){
+            $this->conn = null;
+        }
+    }
+
+    public function getConnection(){
+        return $this->conn;
+    }
+}
+
+
+
+?>
