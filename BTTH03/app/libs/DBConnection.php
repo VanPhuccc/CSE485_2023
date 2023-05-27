ee979a0aca8b1f3435f73d66b7b3f23576b9a36e
Khanh Nguyen <132930424+khanhnyuen@users.noreply.github.com>
2023-05-27 22:35:45 +0700

    Update DBConnection.php

--- BTTH03/app/libs/DBConnection.php
+++ BTTH03/app/libs/DBConnection.php
@@ -1,6 +1,29 @@
 <?php
 
-mysql_connect('localhost','root','') or die('Cannot connect to server');
-mysql_select_db('attsystem') or die ('Cannot found database');
+class DBConnection{
+    private $host;
+    private $user;
+    private $dbname;
+    private $pass;
+    private $conn;
+
+    public function __construct($host,$user,$dbname,$pass)
+    {
+        $this->host = DB_HOST;
+        $this->user = DB_USER;
+        $this->pass = DB_PASS;
+        $this->dbname = DB_NAME;
+
+        try{
+            $this->conn = new PDO("mysql:host={$this->host};dbname={$this->dbname},$this->user,$this->pass");
+        }catch(PDOException $e){
+            $this->conn = null;
+        }
+    }
+
+    public function getConnection(){
+        return $this->conn;
+    }
+}
 
 ?>
