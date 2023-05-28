80e6b428836220be33e29619f0dbcf0fc679d935
Khanh Nguyen <132930424+khanhnyuen@users.noreply.github.com>
2023-05-28 20:50:52 +0700

    Update update_article.php

---
+++ BTTH03/app/views/article/update_article.php
@@ -0,0 +1,20 @@
+<?php 
+	
+		include ('../dbcon.php');
+
+    $id = $_POST['sid'];
+		$name = $_POST['name'];
+		$gender = $_POST['gender'];
+		$std = $_POST['std'];
+		$qry = " UPDATE `student` SET `if` = '$if', `name` = '$name', `gender` = '$gender', WHERE `id` = '$id'; ";
+
+		$run = mysqli_query($con,$qry);
+		
+		if( $run == true) { 
+				?>
+
+				<script type="text/javascript"> alert("Luu du lieu thanh cong!");
+				</script>
+
+				<?php 
+		}
