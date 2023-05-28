04f4a353bca0a2c55e23af6f5ba3bccf6f3088e1
Văn Phúc <71745283+VanPhuccc@users.noreply.github.com>
2023-05-28 18:38:52 +0700

    Update ArticleServices.php

--- BTTH03/app/services/ArticleServices.php
+++ BTTH03/app/services/ArticleServices.php
@@ -1,1 +1,59 @@
+<?php
+include('../app/libs/DBConnection.php');
+include('../app/models/Article.php');
+class ArticleService{
+    public function getallArticle()
+    {
+        $dbconnection = new DBConnection();
+        $conn = $dbconnection->getConnection();
+        if($conn != null)
+        {
+            $sql = "SELECT*from article order by id desc";
+            $stmt= $conn->query($sql);
+            $articles = [];
+            while($rows = $stmt->fetch())
+                {
+                    $article = new Article($rows['id'],$rows['title'],$rows['summary'],$rows['content']);
+                    $articles[] = $article;
+                }
+            return $articles;
+        }
+    }
+
+    public function addArticle($title,$summary,$content)
+    {
+        $dbconnection = new DBConnection();
+        $conn = $dbconnection->getConnection();
+        if($conn !=null)
+        {
+            $sql = "INSERT into article (title,summary,categories) values ('$title','$summary','$content')";
+            $stmt = $conn->query($sql);
+            if($stmt){
+                return true;
+            }else{
+                return false;
+            }
+        }
+    }
+
+    public function findArticlebyID($id){
+        $dbconnection = new DBConnection();
+        $conn = $dbconnection->getConnection();
+        if($conn !=null)
+        {
+            $sql = "SELECT *from article where id = '$id'";
+            $stmt = $conn->query($sql);
+            $articles = [];
+            while($rows = $stmt->fetch())
+                {
+                    $article = new Article($rows['id'],$rows['title'],$rows['summary'],$rows['content']);
+                    $articles[] = $article;
+                }
+            return $articles;
+        }
+    }
+}
+
+
+?>
 
