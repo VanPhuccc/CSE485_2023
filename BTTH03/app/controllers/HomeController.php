649aa419a3023330ddcdbf3c740fe917b26d6cea
Văn Phúc <71745283+VanPhuccc@users.noreply.github.com>
2023-05-28 22:42:22 +0700

    Update HomeController.php

--- BTTH03/app/controllers/HomeController.php
+++ BTTH03/app/controllers/HomeController.php
@@ -1,1 +1,13 @@
+<?php
+include('../app/services/ArticleSevices.php');
+include('../app/config/config.php');
+class HomeController{
+    public function index()
+    {
+        $articleService= new ArticleServices();
+        $article = $articleService->getallArticle();
+        include('../app/views/home/index.php');
+    }
+}
 
+?>
