9a89f0b3b97e0a7775e67c51b9fe2da0e052708a
Văn Phúc <71745283+VanPhuccc@users.noreply.github.com>
2023-05-28 18:43:06 +0700

    Update index.php

--- BTTH03/public/index.php
+++ BTTH03/public/index.php
@@ -1,1 +1,17 @@
+<?php
+
+include('../app/config/config.php');
+    $controller = isset($_GET['controller'])?$_GET['controller']:'home';
+    $action     = isset($_GET['action'])?$_GET['action']:'index';
+    $controller = ucfirst($controller);
+    $controller .= 'Controller';
+    $controllerPath = 'app/controllers/'.$controller.'.php';
+    if(!file_exists($controllerPath)){
+        die('Tệp tin không tồn tại');
+    }
+    require($controllerPath);
+    $myObj = new $controller();
+    $myObj->$action();
+
+?>
 
